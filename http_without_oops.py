490bfac17e80681842eb77f5ff205d5188098fa1
ashwinin5 <ashwinin@vedams.com>
2017-04-19 16:00:19 +0530

    Added two files

---
+++ http_without_oops.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+
+
+driver = webdriver.Firefox()
+
+host = "http://10.1.25.133/v3/index.html"
+username = "manage"
+password = "!manage"
+driver.get(host)
+sleep(5)
+driver.find_element(By.ID, "username").send_keys(username)
+driver.find_element(By.ID, "password").send_keys(password)
+driver.find_element(By.ID, "loginButton").click()
+sleep(8)
+driver.find_element(By.ID, "MtopicsIsystem").click()
+sleep(4)
+driver.find_element(By.ID, "MtopicsIvolumes").click()
+sleep(4)
+driver.find_element(By.ID, "MtopicsIhosts").click()
+sleep(4)
+#driver.find_element(By.ID, "adp_ok").click()
+driver.find_element(By.ID, "logoutButton").click()
+driver.find_element(By.ID, "adp_ok").click()
+driver.close()
